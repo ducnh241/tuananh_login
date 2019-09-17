9f6456f48659f507db40aecf83b88d08ffc5c7a6
ducnhsmcorp <huuductin1k12@gmail.com>
2019-09-17 15:04:55 +0700

    remove license key

--- includes/validate.php
+++ includes/validate.php
@@ -3,12 +3,12 @@
 $server_name = $_SERVER['SERVER_NAME'];
 $license = LICENSE;
 
-if ($license) {
-    if (md5(md5($server_name)) == $license){
-        //continue
-    }else{
-        die('Not permission');
-    }
-} else {
-    die('Not permission');
-}
+//if ($license) {
+//    if (check_license_key(md5($server_name)) == $license){
+//        //continue
+//    }else{
+//        die('Not permission');
+//    }
+//} else {
+//    die('Not permission');
+//}
