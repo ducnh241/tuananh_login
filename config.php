286d9da6afb9975cdc186d22adafb72218f69071
ducnhsmcorp <huuductin1k12@gmail.com>
2019-09-17 15:22:48 +0700

    add dogcument

--- config.php
+++ config.php
@@ -3,5 +3,5 @@ error_reporting(0);
 define('PASS_WORD', 'nguyenduc@123');
 define('HOTLINE', '0968.057.988');
 define('LICENSE', '4e24674842b987fe643e34710fd2daa2');
-define('GOOGLE_SHEET_ID', '1FvdN5aZ0ItvN1tFkh79y1uYhiReCfTFTFDtEPptbWI4');
+define('GOOGLE_SHEET_ID', '1qrW4pq0xxvRJPx6MOH2xeIjJGoIERbQ_mu3LjmV1OLg');
 define('URL_INDEX', 'http://facebook.com');
